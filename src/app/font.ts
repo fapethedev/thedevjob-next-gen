607980eded95c0577ad784ba65a0a6b29ffb44c5
fapethedev <mpetrivlin@gmail.com>
2024-12-15 13:52:50 +0000

    Update: setting up orbitron, exo2, bebasNeue subsets to latin

--- src/app/font.ts
+++ src/app/font.ts
@@ -2,16 +2,19 @@ import {Bebas_Neue, Exo_2, Orbitron} from "next/font/google";
 
 const orbitron = Orbitron({
     style: ["normal"],
+    subsets: ["latin"],
     weight: ["400", "500", "600", "700", "800", "900"]
 });
 
 const exo2 = Exo_2({
     style: ["normal", "italic"],
+    subsets: ["latin"],
     weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
 });
 
 const bebasNeue = Bebas_Neue({
     style: ["normal"],
+    subsets: ["latin"],
     weight: ["400"]
 });
 
