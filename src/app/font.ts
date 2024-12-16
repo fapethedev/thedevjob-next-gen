168cafcf4ac1c642effce875680f957b7b29a4dc
fapethedev <mpetrivlin@gmail.com>
2024-12-16 18:45:27 +0000

    Fix: Font loading keep fail du to a force preload

--- src/app/font.ts
+++ src/app/font.ts
@@ -3,19 +3,22 @@ import {Bebas_Neue, Exo_2, Orbitron} from "next/font/google";
 const orbitron = Orbitron({
     style: ["normal"],
     subsets: ["latin"],
-    weight: ["400", "500", "600", "700", "800", "900"]
+    weight: ["400", "500", "600", "700", "800", "900"],
+    preload: false
 });
 
 const exo2 = Exo_2({
     style: ["normal", "italic"],
     subsets: ["latin"],
-    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+    preload: false
 });
 
 const bebasNeue = Bebas_Neue({
     style: ["normal"],
     subsets: ["latin"],
-    weight: ["400"]
+    weight: ["400"],
+    preload: false
 });
 
 export {orbitron, exo2, bebasNeue};
