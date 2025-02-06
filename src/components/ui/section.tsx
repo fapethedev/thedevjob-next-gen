c6650e1db86e042df237f8c6bcb2661e46c25261
fapethedev <mpetrivlin@gmail.com>
2025-02-06 14:16:42 +0000

    Update: adds GlobeSection component in section.tsx

--- src/components/ui/section.tsx
+++ src/components/ui/section.tsx
@@ -1,7 +1,16 @@
 'use client';
 
 import React from "react";
+import {motion} from "framer-motion";
+
 import {cn} from "@/lib/utils";
+
+
+interface GlobeSectionProps extends React.HTMLAttributes<HTMLElement> {
+    title: string;
+    description: string;
+    children?: React.ReactNode;
+}
 
 
 const Section = React.forwardRef<
@@ -34,6 +43,46 @@ const HeroSection = React.forwardRef<
 
 HeroSection.displayName = 'HeroSection';
 
+const GlobeSection = React.forwardRef<
+    HTMLElement,
+    GlobeSectionProps
+>(({className, children, title, description, ...props}, ref) => (
+    <Section
+        ref={ref}
+        className={cn("justify-center items-center z-0 flex flex-row bg-transparent relative w-full", className)}
+        {...props}
+    >
+        <div className="mx-auto w-full relative overflow-hidden h-full md:h-[56rem] px-4">
+                <motion.div
+                    initial={{
+                        opacity: 0,
+                        y: 20,
+                    }}
+                    animate={{
+                        opacity: 1,
+                        y: 0,
+                    }}
+                    transition={{
+                        duration: 1,
+                    }}
+                    className="div"
+                >
+                    <h2 className="text-center text-4xl md:text-5xl font-bold text-primary-alt dark:text-slate-300">
+                        {title}
+                    </h2>
+                    <p className="text-center text-base md:text-lg font-normal text-primary dark:text-slate-200 max-w-md mt-2 mx-auto">
+                        {description}
+                    </p>
+                </motion.div>
+               <div className="w-full h-72 md:h-[48rem] z-10">
+                    {children}
+                </div>
+            </div>
+    </Section>
+));
 
-export {Section, HeroSection};
+GlobeSection.displayName = 'GlobeSection';
+
+
+export {Section, HeroSection, GlobeSection};
 
