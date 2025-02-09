09e3cd1cc6e2ad92974f7242b8ed3a270b41fba1
fapethedev <mpetrivlin@gmail.com>
2025-02-09 18:18:12 +0000

    Update: Add link to dialog footer button to download cv on blank page

--- src/components/ui/mobile-nav.tsx
+++ src/components/ui/mobile-nav.tsx
@@ -1,13 +1,14 @@
-import {FaBars} from "react-icons/fa6";
-import {Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger} from "@/components/ui/dialog";
+import {useState} from "react";
 import Link from "next/link";
+import {FaBars} from "react-icons/fa6";
+
+import {Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger} from "@/components/ui/dialog";
 import {Button} from "@/components/ui/button";
 import Logo from "@/components/ui/logo";
 import {Separator} from "@/components/ui/separator";
 import {cn} from "@/lib/utils";
 import {orbitron} from "@/app/font";
 import {ModeToggle} from "@/components/ui/mode-toggle";
-import {useState} from "react";
 
 
 export default function MobileNav({ navigation, pathname }: {navigation: {name: string, href: string}[], pathname: string}) {
@@ -27,22 +28,26 @@ export default function MobileNav({ navigation, pathname }: {navigation: {name: 
                         <Logo/>
                     </DialogTitle>
                 </DialogHeader>
-                <Separator />
+                <Separator/>
                 <div className="w-full h-full flex flex-col gap-y-4">
                     {navigation.map((item) => (
-                    <Link
-                        key={item.name}
-                        href={item.href}
-                        onClick={handleCloseDialog}
-                        className={`${item.href === pathname && "border p-2 border-primary-alt dark:border-secondary-alt rounded"} text-primary-alt dark:text-secondary uppercase font-semibold md:my-auto`}
-                    >
-                        {item.name}
-                    </Link>
-                ))}
+                        <Link
+                            key={item.name}
+                            href={item.href}
+                            onClick={handleCloseDialog}
+                            className={`${item.href === pathname && "border p-2 border-primary-alt dark:border-secondary-alt rounded"} text-primary-alt dark:text-secondary uppercase font-semibold md:my-auto`}
+                        >
+                            {item.name}
+                        </Link>
+                    ))}
                     <ModeToggle/>
                 </div>
                 <DialogFooter>
-                    <Button variant="default" className="w-full font-bold uppercase md:text-5xl md:my-auto md:p-12">Download CV</Button>
+                    <Button variant="default" className="w-full font-bold uppercase md:text-5xl md:my-auto md:p-12">
+                        <Link href="#" download target="_blank">
+                            Download CV
+                        </Link>
+                    </Button>
                 </DialogFooter>
             </DialogContent>
         </Dialog>
