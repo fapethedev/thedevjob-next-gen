cf885eff0228c4e62b10f8f1f8d78389d4c4cc81
fapethedev <mpetrivlin@gmail.com>
2025-02-13 09:53:36 +0000

    Update: Refactor and add social media component

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,12 +1,10 @@
-import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger,} from "@/components/ui/tooltip"
 import {Separator} from "@/components/ui/separator";
 import Logo from "@/components/ui/logo";
-import {FaGithub, FaLinkedinIn, FaUpwork, FaWhatsapp} from "react-icons/fa6";
-import {PiGearDuotone} from "react-icons/pi";
 import Link from "next/link";
 import {cn} from "@/lib/utils";
 import {exo2, orbitron} from "@/app/font";
 import {HiDocumentDownload} from "react-icons/hi";
+import SocialMedia from "@/components/ui/social-media";
 
 const FooterTop = () => (
     <div className="flex flex-col gap-y-24">
@@ -62,62 +60,7 @@ const FooterBottom = () => (
         <div className="flex items-start justify-start">
             <Logo className="lg:-m-1.5 lg:p-1.5"/>
         </div>
-        <div
-            className="flex items-center justify-center py-8 gap-x-9 text-4xl text-primary-alt dark:text-secondary md:py-10 md:gap-x-10 md:text-4xl lg:py-4 lg:gap-x-12 lg:text-3xl"
-        >
-            <TooltipProvider>
-                <Tooltip>
-                    <TooltipTrigger>
-                        <Link href="https://www.linkedin.com/in/abiola-fatigba-a0532a27b/" target="_blank">
-                            <FaLinkedinIn/>
-                        </Link>
-                    </TooltipTrigger>
-                    <TooltipContent className={cn(orbitron.className, "bg-primary-alt text-slate-50 text-xs/2 dark:bg-secondary dark:text-primary-alt uppercase")}>
-                        <p>LinkedIn</p>
-                    </TooltipContent>
-                </Tooltip>
-                <Tooltip>
-                    <TooltipTrigger>
-                        <Link href="https://www.upwork.com/freelancers/" target="_blank">
-                            <FaUpwork/>
-                        </Link>
-                    </TooltipTrigger>
-                    <TooltipContent className={cn(orbitron.className, "bg-primary-alt text-slate-50 text-xs/2 dark:bg-secondary dark:text-primary-alt uppercase")}>
-                        <p>UpWork</p>
-                    </TooltipContent>
-                </Tooltip>
-                <Tooltip>
-                    <TooltipTrigger>
-                        <Link href="https://www.usebraintrust.com/" target="_blank">
-                            <PiGearDuotone/>
-                        </Link>
-                    </TooltipTrigger>
-                    <TooltipContent className={cn(orbitron.className, "bg-primary-alt text-slate-50 text-xs/2 dark:bg-secondary dark:text-primary-alt uppercase")}>
-                        <p>Braintrust</p>
-                    </TooltipContent>
-                </Tooltip>
-                <Tooltip>
-                    <TooltipTrigger>
-                        <Link href="https://github.com/fapethedev/" target="_blank">
-                            <FaGithub/>
-                        </Link>
-                    </TooltipTrigger>
-                    <TooltipContent className={cn(orbitron.className, "bg-primary-alt text-slate-50 text-xs/2 dark:bg-secondary dark:text-primary-alt uppercase")}>
-                        <p>Github</p>
-                    </TooltipContent>
-                </Tooltip>
-                <Tooltip>
-                    <TooltipTrigger>
-                        <Link href="/" target="_blank">
-                            <FaWhatsapp/>
-                        </Link>
-                    </TooltipTrigger>
-                    <TooltipContent className={cn(orbitron.className, "bg-primary-alt text-slate-50 text-xs/2 dark:bg-secondary dark:text-primary-alt uppercase")}>
-                        <p>Whatsapp</p>
-                    </TooltipContent>
-                </Tooltip>
-            </TooltipProvider>
-        </div>
+        <SocialMedia/>
     </div>
 );
 
