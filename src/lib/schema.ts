6a6acbe6a40d722eb8c6fd485589ec04ac235da3
fapethedev <mpetrivlin@gmail.com>
2025-02-13 09:51:14 +0000

    Update: A schema for contact form

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -9,3 +9,14 @@ export const NewsletterFormSchema = z.object({
 });
 
 export type NewsletterFormValues = z.infer<typeof NewsletterFormSchema>;
+
+
+export const ContactFormSchema = z.object({
+    fullName: z.string().trim().min(2, "Full name must be at least 2 characters"),
+    email: z.string().trim().toLowerCase().email("Invalid email address"),
+    subject: z.string().trim().min(5, "Subject must be at least 5 characters"),
+    message: z.string().trim().min(10, "Message must be at least 10 characters"),
+    newsletter: z.boolean().default(false),
+});
+
+export type ContactFormValues = z.infer<typeof ContactFormSchema>;
